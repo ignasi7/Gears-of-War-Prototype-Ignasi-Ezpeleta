bffc8759ed9145fe75ed42783be5f6cae7dfa624
ignasi7 <ignasiezpeleta@gmail.com>
2024-08-06 17:58:22 +0200

    Project creation

---
+++ Source/TechnicalExercise/TechnicalExercise.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TechnicalExercise.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TechnicalExercise, "TechnicalExercise" );
+ 
---
+++ Source/TechnicalExercise/TechnicalExerciseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TechnicalExerciseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATechnicalExerciseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATechnicalExerciseGameMode();
+};
+
+
+
